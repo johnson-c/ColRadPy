c209703c084ba97a0daf89e15f301c3473ccb28e
johnson-c <caj0026@auburn.edu>
2018-03-15 11:39:22 -0500

    Added read_adf04.py and ionization.py

---
+++ ionization_balance.py
@@ -0,0 +1,205 @@
+import numpy as np
+from scipy.linalg import qr
+import scipy
+from scipy import linalg, matrix
+
+def qr_null(A, tol=None):
+    Q, R, P = qr(A.T, mode='full', pivoting=True)
+    tol = np.finfo(R.dtype).eps if tol is None else tol
+    rnk = min(A.shape) - np.abs(np.diag(R))[::-1].searchsorted(tol)
+    return Q[:, rnk:].conj()
+def null(A, eps=1e-15):
+    u, s, vh = scipy.linalg.svd(A)
+    null_mask = (s <= eps)
+    null_space = scipy.compress(null_mask, vh, axis=0)
+    return scipy.transpose(null_space)
+def null(A, eps=1e-1):
+    u, s, vh = scipy.linalg.svd(A)
+    padding = max(0,np.shape(A)[1]-np.shape(s)[0])
+    null_mask = np.concatenate(((s <= eps), np.ones((padding,),dtype=bool)),axis=0)
+    null_space = scipy.compress(null_mask, vh, axis=0)
+    return scipy.transpose(null_space)
+
+
+def null(A, atol=1e-13, rtol=0):
+    A = np.atleast_2d(A)
+    u, s, vh = np.linalg.svd(A)
+    tol = max(atol, rtol * s[0])
+    nnz = (s >= tol).sum()
+    ns = vh[nnz:].conj().T
+    return ns
+
+
+
+
+
+'''
+charge_state_files = np.array(['/home/curtis/spirit/adf04-10apr17_mod'])
+charge_state_files = np.array(['/home/curtis/spirit/wdarc7_mod'])
+charge_state_files = np.array(['/home/curtis/spirit/wlike_mons11#w0.dat'])
+charge_state_files =  '/home/curtis/Downloads/adf04_WI_16.10.17_ecip'
+charge_state_files = '/home/curtis/spirit/wdx/tdcc/adf04_WI_5.12-3.17_tdcc'
+
+charge_state_files = '/home/curtis/spirit/mo_i_figure_390nm_connors_paper/adf04'
+#np.array(['/home/curtis/lewis/he_0/mom97_ls#he0.dat','/home/curtis/lewis/he_0/mom97_ls#he1.dat']) 
+
+
+#sig = np.array([0,1])
+#sig = np.linspace(0,39,40,dtype='int')
+sig = np.array([0,1])
+nsigmas = np.array([len(sig)])
+gcrs = []
+
+temperature_grid = np.array([6])#temp_arr
+electron_den = np.array([1.e11]) #dens_arr
+
+#for i in range(0,len(charge_state_files)):
+
+'''
+
+def ioniz(gc):
+    gcrs=[]
+    nsigmas = np.array([len(gc['metas'])])
+    sig = gc['metas']
+    gcrs.append(gc)
+    temperature_grid = gc['user_temp_grid']
+    electron_den = gc['user_dens_grid']
+
+    ionbal = np.zeros((np.sum(nsigmas)+1,np.sum(nsigmas)+1,
+                       len(gcrs[0]['user_temp_grid']),
+                       len(gcrs[0]['user_dens_grid'])))
+
+    '''
+    i = 0
+    for k in range(0, np.sum(nsigmas)):
+        if(k > np.sum(nsigmas[0:i])):
+            i = i + 1
+        for j in range(0,np.sum(nsigmas)):
+            print(k,j,i)
+
+            if(k != j):
+                if(gcrs[i]['qcd'].any() and j <len(gcrs[i]['qcd']) ):
+                    print('here')
+                    ionbal[k,j,:,:] = gcrs[i]['qcd'][j,k,:,:]
+            else:
+
+            ionbal[k,j,:,:] = -1*np.sum(gcrs[i]['scd'][j,:,:,:],axis=1)
+            if(gcrs[i]['qcd'].any()):
+                ionbal[k,j,:,:] = ionbal[k,j,:,:] + -1*np.sum(gcrs[i]['qcd'][j,:,:,:],axis=1)
+    '''
+    i = 0
+    p = 0
+    q = 0
+    r = 0
+    s = 0
+    t = 0
+
+    for k in range(0, np.sum(nsigmas)):
+        if(k > np.sum(nsigmas[0:i+1]-1)):
+            i = i + 1
+            p = 0
+            q = 0
+            r = 0
+        for j in range(0,np.sum(nsigmas)):
+
+            #put the -qcd on the diagonal
+            if( k==j and j < np.sum(nsigmas) and gcrs[i]['qcd'].any()):
+                ionbal[k,j,:,:] = ionbal[k,j,:,:] + -1*(np.sum(gcrs[i]['qcd'][p,:,:,:],axis=0))
+                p = p + 1
+
+            if( k!=j and gcrs[i]['qcd'].any()):
+                if(k<nsigmas[i] and j <nsigmas[i]):
+                    if(k>j):
+                        ionbal[k,j,:,:] = ionbal[k,j,:,:] + gcrs[i]['qcd'][j,k-1,:,:]
+                    else:
+                        ionbal[k,j,:,:] = ionbal[k,j,:,:] + gcrs[i]['qcd'][j,k,:,:]                    
+
+            if( k!=j and gcrs[s]['scd'].any() and (k>np.sum(nsigmas[0:s+1])-1 or j >np.sum(nsigmas[0:s+1])-1) ):
+                if(k < j):
+                    ionbal[k,j,:,:] = ionbal[k,j,:,:] + gcrs[s]['acd'][k,j-nsigmas[s],:,:]
+                if(k > j):
+                    ionbal[k,j,:,:] = ionbal[k,j,:,:] + gcrs[s]['scd'][j,k-nsigmas[s],:,:]
+
+            #put the -scd on the diagonal
+            if( k==j and gcrs[i]['scd'].any()):
+                ionbal[k,j,:,:] = ionbal[k,j,:,:] + -1*(np.sum(gcrs[i]['scd'][q,:,:,:],axis=0))
+                q = q + 1
+
+            #put the -acd on the diagonal
+            if( k==j and gcrs[i]['acd'].any() and k >= nsigmas[0]-1):
+
+                ionbal[np.sum(nsigmas[0:i+1]),np.sum(nsigmas[0:i+1]),:,:] = ionbal[np.sum(nsigmas[0:i+1]),np.sum(nsigmas[0:i+1]),:,:]\
+                -1*(np.sum(np.sum(gcrs[i]['acd'][:,:,:,:],axis=0),axis=0))
+
+                r = r+ 1
+
+    u = 0            
+    for k in range( np.sum(nsigmas) - nsigmas[-1]  ,np.sum(nsigmas)):
+        ionbal[-1,k,:,:] = ionbal[-1,k,:,:] + gcrs[-1]['scd'][u,0,:,:]
+        ionbal[k,-1,:,:] = ionbal[k,-1,:,:] + gcrs[-1]['acd'][u,0,:,:]
+
+        u = u+1
+
+
+
+    #include source term
+
+
+
+
+
+
+
+
+    tmp = np.ones_like(ionbal[:,:,0,0][0,:])
+
+    ionbal_tosolve = np.copy(ionbal)
+    for t in range(0,len(gcrs[0]['user_temp_grid'])):
+        for n in range(0,len(gcrs[0]['user_dens_grid'])):
+            ionbal_tosolve[0,:,t,n ] = tmp
+
+
+    b_mat = np.zeros_like(ionbal[:,:,0,0][0,:])
+    b_mat[0] = 1
+
+
+    ##########################################################################################
+    #
+    # time depepent part
+    #
+    ##########################################################################################
+
+    t_min = 0
+    t_max = .8
+    dt = (10*np.max(ionbal))
+    nsteps = (t_max - t_min) /dt
+    t_steps = np.linspace(t_min,t_max,nsteps)
+
+    td_pop = np.zeros( (np.shape(t_steps) + np.shape(ionbal[0]) ))
+    td_pop[0,0,:,:] = 1
+
+    for t in range(0,len(gcrs[0]['user_temp_grid'])):
+        for n in range(0,len(gcrs[0]['user_dens_grid'])):
+            for i in range(1, len(t_steps)):
+                td_pop[i,:,t,n] = td_pop[i-1,:,t,n] + (1/dt)*np.dot(ionbal[:,:,t,n],td_pop[i-1,:,t,n])
+
+
+
+
+    ##########################################################################################
+    #
+    # time depepent part with source
+    #
+    ##########################################################################################
+
+    source =1.e1
+
+    td_pop_s = np.zeros( (np.shape(t_steps) + np.shape(ionbal[0]) ))
+
+    source_arr = np.zeros(len(sig)+1)
+    source_arr[0] = source*dt
+    for t in range(0,len(gcrs[0]['user_temp_grid'])):
+        for n in range(0,len(gcrs[0]['user_dens_grid'])):
+            for i in range(1, len(t_steps)):
+                td_pop_s[i,:,t,n] = td_pop_s[i-1,:,t,n] + (1/dt)*np.dot(ionbal[:,:,t,n],td_pop_s[i-1,:,t,n]) + source_arr 
+    return t_steps,td_pop,td_pop_s
---
+++ read_adf04.py
@@ -0,0 +1,345 @@
+################################################################################
+# file name         : read_adf04.py
+# author            : Curt Johnson
+# description       : This code reads adf04 formatted files
+# version           : 0.1
+# python version    : 2.7.12 ipython 2.4.1
+# dependencies      : numpy, re
+# license           : to be freed after code is stable and papers are published
+#
+# todo          check with all the adf04 files in adas
+#
+#
+################################################################################
+
+import numpy as np
+import re
+from operator import xor
+
+def read_adf04(file_path):
+    file_name = file_path
+    dict = {}
+    f = open(file_name)
+
+    ############################################################################
+    #
+    # reading the first line
+    #
+    ############################################################################
+    first_line = re.split('[ ]', f.readline())
+
+
+    for i in range(0,len(first_line)):
+
+        if(first_line[i] != '' and  not dict.has_key('element')):
+            dict['element'] = first_line[i]
+            if(len( dict['element']) > 3):
+                tt = re.split('[+]',dict['element'])[0]
+                dict['element'] = tt[0]
+                dict['charge_state'] = tt[1]
+        elif(first_line[i] != '' and  dict.has_key('element') and \
+             not dict.has_key('charge_state') and first_line[i] !='+'):
+            dict['charge_state'] = first_line[i]
+
+        elif(first_line[i] != '' and  dict.has_key('element') and \
+           dict.has_key('charge_state') and not dict.has_key('iz0')):
+
+            dict['iz0'] = first_line[i]
+        elif(first_line[i] != '' and  dict.has_key('element') and \
+           dict.has_key('charge_state') and dict.has_key('iz0') and \
+           not dict.has_key('iz1')):
+            dict['iz1'] = first_line[i]
+        elif(first_line[i] != '' and  dict.has_key('element') and \
+           dict.has_key('charge_state') and dict.has_key('iz0') and \
+           dict.has_key('iz1') and not dict.has_key('ion_term')):
+            dict['ion_pot'] = float(re.split('[(]',first_line[i])[0])
+            dict['ion_term'] = re.split('[)]',re.split('[(]',
+                                                       first_line[i])[1])[0]
+
+    ############################################################################
+    #
+    # done reading the first line, reading the level information
+    #
+    ############################################################################
+
+    dict['charge_state'] = int(dict['charge_state'])
+    config = []
+    L = []
+    S = []
+    w = []
+    energy = []
+    zpla = []
+    neg_found = False
+    level_ind_found = False
+    conf_temp = ''
+    conf_tmp_found = False
+    maybe_w=''
+    ttttmp = f.readline()
+    level_line = re.split('[ ]',ttttmp)
+    while(  xor( ('-1\n' not in level_line),(level_line[0] == 'C') )):
+        neg_found = False
+        level_ind_found = False
+        conf_temp = ''
+        conf_tmp_found = False
+        maybe_w=''
+        energy_found = False
+        conf_tmp = ''
+        import pdb
+
+        for i in range(0,len(level_line)):
+
+            if(level_line[i] != '' and  not level_ind_found ):
+                level_ind_found = True
+            elif(level_line[i] != '' and  level_ind_found and\
+                 level_line[i][0] != '(' and not conf_tmp_found):
+
+                conf_tmp = conf_tmp + level_line[i] + ' '
+            elif(level_line[i] != '' and  level_ind_found and\
+                 level_line[i][0] == '('):
+                conf_tmp_found = True
+                sl = re.split('[(]',level_line[i])[1]
+                S.append( int(re.split('[)]',sl)[1]))
+                L.append( int(re.split('[)]',sl)[0]))
+                maybe_w = re.split('[(]',level_line[i])[-1]
+                if(maybe_w):
+                    w.append(float(re.split('[)]',maybe_w)[0]))
+            elif(not maybe_w and conf_tmp_found and level_ind_found
+                 and level_line[i] != ''):
+                if(level_line[i][-1] == ')'):
+                    w.append(float(re.split('[)]',level_line[i])[0]))
+                    maybe_w = 'found'
+            elif( maybe_w and conf_tmp_found and level_ind_found and
+                  level_line[i]!='' and level_line[i][0]!='{' and not energy_found):
+                energy.append(float(level_line[i]))
+                energy_found = True
+            elif( maybe_w and conf_tmp_found and level_ind_found and
+                  level_line[i]!='' and level_line[i][0] == '{'):
+                if(level_line[i][1]=='' or level_line[i][1] =='X'):
+                    zpla.append(-1)
+                else:
+                    zpla.append( float(re.split('[}]',level_line[i])[1]))
+        config.append(conf_tmp[0:len(conf_tmp)-1])
+        ttttmp = f.readline()        
+        level_line = re.split('[ ]',ttttmp)
+    ############################################################################
+    #
+    # end reading level info start reading temperature grid
+    #
+    ############################################################################
+    t = f.readline()
+    temp_grid_tmp = re.split('[ ]',t)
+    one = False
+    two = False
+    adf04_temp_grid = []
+    for i in range(0,len(temp_grid_tmp)):
+
+        if(temp_grid_tmp[i] !='' and one and two and temp_grid_tmp[i] !='\n'):
+            if( '+' in temp_grid_tmp[i]):
+                temp_grid_tmp[i] = temp_grid_tmp[i].replace('+','E')
+            elif('-' in temp_grid_tmp[i]):
+                temp_grid_tmp[i] = temp_grid_tmp[i].replace('-','E-')
+            adf04_temp_grid.append(float(temp_grid_tmp[i]))
+
+        if(temp_grid_tmp[i] !='' and one and not two):
+            two = True
+
+        if(temp_grid_tmp[i] !='' and not one):
+            one = True
+
+    dict['temp_grid'] = np.asarray(adf04_temp_grid)
+    dict['energy'] =np.asarray(energy)
+    dict['L'] = np.asarray(S)
+    dict['S'] = np.asarray(L)
+    dict['w'] = np.asarray(w)
+    dict['config'] = np.asarray(config)
+    dict['zpla'] = np.asarray(zpla)
+    ################################################################################
+    #
+    # end reading temp grid start reading in the atomic data from the adf04 file
+    #
+    ################################################################################
+    a = np.loadtxt(file_name,skiprows=len(L)+3,dtype=str,delimiter='\n')
+
+    #declare the colisional stuff
+    col_excit = []
+    col_transitions = []
+    col_trans = np.array([-1,-1])
+    #declare the recombination stuff
+    recomb_excit = []
+    recomb_transitions = []
+    recomb_trans = np.array([-1,-1])
+    #declare the ion stuff
+    ion_excit = []
+    ion_transitions = []
+    ion_trans = np.array([-1,-1])
+    #declare the a value stuff
+    a_val = []
+    inf_engy= []
+
+    i = 0 #this is the index of the line in the adf04 file starting at the colision
+    b = re.split('[ ]',a[i])#this will the the line that gets read from the loadtxt
+
+    while( '-1' not in b):
+
+        #################################################
+        #
+        # get the excitation and a values from the adf04
+        #
+        ##################################################
+
+        first_ind = -1  #tells if first index found
+        second_ind = -1 #tells if second index found
+        if(b[0] == ''): #this means that it is collisional excitation and A vals
+            k = 0
+            #going to loop over to keep looping until the indices are found
+            #this is happending one ' ' at a time
+            while(second_ind != 1): #for k in range(1,len(b)):
+                #check for the second index first 
+                if(b[k] !='' and first_ind==1 and second_ind == 0):
+
+                    col_trans[1] = int(b[k])
+                    second_ind=1
+                #check if the first index found
+                if(b[k] !='' and first_ind ==-1):
+                    col_trans[0] = int(b[k])
+                    first_ind = 1
+                    second_ind=0
+                k = k + 1 # that ' ' step
+            col_transitions.append(np.array([col_trans[0],col_trans[1]]))#append the indexes
+            problem_w_b = np.where(np.asarray(b[k:]) =='')[0]
+            if(len(problem_w_b) > 0):
+                b = b[k:problem_w_b[0]]
+            else:
+                b = b[k:] #now b is just the aval and the upsilons
+            c = np.zeros(len(b)) #make a matrix for the upsilons
+            if(len(b) <len(adf04_temp_grid)+2):
+
+                tmpp = b[-1][7:]
+                b[-1] = b[-1][0:7]
+                b.append(tmpp)
+
+
+            #this is because adas has poor scientific notation
+            for j in range(0,len(adf04_temp_grid)+1): # +1 because the a vale in there too
+                if( '+' in b[j]):
+                    b[j] = b[j].replace('+','E')
+                elif('-' in b[j]):
+                    b[j] = b[j].replace('-','E-')
+                c[j] =float(b[j])
+            a_val.append(c[0])#append the A values
+            col_excit.append(c[1:1+len(adf04_temp_grid)])#append the upsilon matrix
+
+
+            b = np.asarray(b)
+            if(np.where(b == '')[0]):
+                b = np.delete(b,np.where(b == '')[0])
+
+            if(len(b) == len(adf04_temp_grid)+2):
+                inf_e = b[-1]
+                l = []
+                l.append(inf_e[0])
+                if( '+' in inf_e[1:]):
+                    l.append(inf_e[1:].replace('+','E'))
+                elif('-' in inf_e[1:]):
+                    l.append(inf_e[1:].replace('-','E-'))
+
+                inf_engy.append(float(''.join(l)))
+                c[j] =float(b[j])
+        ######################################################################
+        #
+        # end excitation and a values from the adf04, start looking for recomb
+        #
+        ######################################################################
+
+        elif(b[0] == 'R'):
+            k = 0
+            recomb_first_ind =  -1
+            recomb_second_ind = -1
+            while(recomb_second_ind != 1):
+                if(b[k] != 'R' and b[k] != '' and recomb_first_ind==1 and recomb_second_ind==0):
+
+                    recomb_trans[0] = int(b[k])
+                    recomb_second_ind = 1
+                if(b[k] != 'R' and b[k] != '' and recomb_first_ind==-1):
+                    recomb_trans[1] = int(b[k])
+                    recomb_first_ind = 1
+                    recomb_second_ind = 0
+                k = k + 1 #that ' ' step
+            recomb_transitions.append(np.array([recomb_trans[0],recomb_trans[1]]))
+
+            b = b[k:]
+            #get rid of '' leading up to the upsilons
+            upsilon_found = False
+            ups_ind = 0 
+            while( not upsilon_found):
+                if(b[ups_ind] != ''):
+                    upsilon_found = True
+                ups_ind = ups_ind + 1
+            b = b[ups_ind-1:]
+
+            c = np.zeros(len(b)) #make a matrix for the upsilons
+            #this is because adas has poor scientific notation
+            for j in range(0,len(b)):
+                if( '+' in b[j]):
+                    b[j] = b[j].replace('+','E')
+                elif('-' in b[j]):
+                    b[j] = b[j].replace('-','E-')
+                c[j] =float(b[j])
+            recomb_excit.append(c)
+
+        elif(b[0] == 'S'):
+            k = 0
+            ion_first_ind =  -1
+            ion_second_ind = -1
+            while(ion_second_ind != 1):
+                if(b[k] != 'R' and b[k] != '' and ion_first_ind==1 and ion_second_ind==0):
+
+                    ion_trans[0] = int(b[k])
+                    ion_second_ind = 1
+                if(b[k] != 'S' and b[k] != '' and ion_first_ind==-1):
+                    ion_trans[1] = int(b[k])
+                    ion_first_ind = 1
+                    ion_second_ind = 0
+                k = k + 1 #that ' ' step
+
+            ion_transitions.append(np.array([ion_trans[1],ion_trans[0]]))
+
+            b = b[k:]
+            #get rid of '' leading up to the upsilons
+            upsilon_found = False
+            ups_ind = 0 
+            while( not upsilon_found):
+                if(b[ups_ind] != ''):
+                    upsilon_found = True
+                ups_ind = ups_ind + 1
+            b = b[ups_ind-1:]
+
+            c = np.zeros(len(b)) #make a matrix for the upsilons
+            #this is because adas has poor scientific notation
+            for j in range(0,len(b)):
+                if( '+' in b[j]):
+                    b[j] = b[j].replace('+','E')
+                    c[j] =float(b[j])
+                elif('-' in b[j]):
+                    b[j] = b[j].replace('-','E-')
+                    c[j] =float(b[j])
+                    
+            ion_excit.append(c)
+        i = i + 1 #incrimententing the next line in the file index
+        b = re.split('[ ]',a[i])#getting the next line in the file done this way
+        #so that we can find '-1' signifying the end of the file before we loop
+
+
+
+
+    a_val = np.asarray(a_val)#make the a vals a np array instead of list
+    #make the upsilons np array instead of list
+    dict['a_val'] = a_val
+    dict['col_transitions'] = np.asarray(col_transitions)
+    dict['recomb_transitions'] = np.asarray(recomb_transitions)
+    dict['ion_transitions'] = np.asarray(ion_transitions)
+    dict['col_excit'] = np.asarray(col_excit)
+    dict['recomb_excit'] = np.asarray(recomb_excit)
+    dict['ion_excit'] = np.asarray(ion_excit)
+    dict['inf_engy'] = np.asarray(inf_engy)
+    return dict
