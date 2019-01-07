3dee07821fde2cb6b3f6ee226c493c5652600d65
Curt Johnson <caj0026@auburn.edu>
2019-01-07 14:20:45 -0600

    Added solution definition so solve_matrix_expontial does not have to be called separetly

--- ionization_balance.py
+++ ionization_balance.py
@@ -1,5 +1,6 @@
 import numpy as np
 from colradpy_class import colradpy
+from solve_matrix_exponential import *
 
 class ionization_balance():
 
@@ -8,16 +9,16 @@ class ionization_balance():
 
         self.ion = []
         self.metas_tot = 0
-        self.temp_grid = temp_grid
-        self.dens_grid = dens_grid
+        self.temp_grid = np.asarray(temp_grid)
+        self.dens_grid = np.asarray(dens_grid)
         for i in range(0,len(fils)):
             self.ion.append(colradpy(fils[i],metas[i],temp_grid,dens_grid,use_ionization=use_ionization,
-                                     suppliment_with_ecip=suppliment_with_ecip, use_recombination_three_body=use_recombination_three_body,use_recombination=use_recombination))
+                                     suppliment_with_ecip=suppliment_with_ecip,
+                                     use_recombination_three_body=use_recombination_three_body,
+                                     use_recombination=use_recombination))
             self.ion[i].solve_cr()
             #self.metas_tot = self.metas_tot + self.ion[i].data['atomic']['metas']
         self.populate_ion_matrix()
-
-
 
     def populate_ion_matrix(self):
 
@@ -26,8 +27,7 @@ class ionization_balance():
             m = m + len(self.ion[i].data['atomic']['metas'])
             if(i == len(self.ion) -1):
                 m = m+len(self.ion[i].data['atomic']['ion_pot'])
-        self.ion_matrix = np.zeros((m, m,
-                                    len(self.temp_grid), len(self.dens_grid)))
+        self.ion_matrix = np.zeros((m, m,len(self.temp_grid), len(self.dens_grid)))
 
         m = 0
         for i in range(0,len(self.ion)):
@@ -44,6 +44,7 @@ class ionization_balance():
                                     +self.ion[i].data['processed']['qcd'].transpose(1,0,2,3)
 
             #populate SCDs in ion balance
+
             self.ion_matrix[m+diag_met[0],m+diag_met[1]] = self.ion_matrix[m+diag_met[0],m+diag_met[1]]\
                                       -np.sum(self.ion[i].data['processed']['scd'],axis=1)
             
@@ -72,7 +73,8 @@ class ionization_balance():
             m = m + num_met
             #self.ion_matrix[m+num_met:m+num_met+num_ion,m:m+num_met,:,:]\
 
-
-
+    def solve_no_source(self,n0,td_t):
+        self.pops,self.eigen_val,self.eigen_vec = solve_matrix_exponential(
+                                       np.einsum('ijkl,l->ijkl',self.ion_matrix,self.dens_grid),n0,td_t)
 
 
