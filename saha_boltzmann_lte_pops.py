859a6fee24e73d2185c430bb5394d2909f561423
Curt Johnson <caj0026@auburn.edu>
2019-06-10 13:09:38 -0500

    updated saha boltzmann not done yet but close

--- saha_boltzmann_lte_pops.py
+++ saha_boltzmann_lte_pops.py
@@ -24,17 +24,23 @@ def boltzmann_population(w,energy,temp,norm_tot=False):
     :returns: float array populations of levels relative to the ground [len(levels),len(temp)]
 
     """
+    
     stat_weight = (w[1:]*2 + 1) / (w[0]*2 + 1)
     boltz_pop = stat_weight[:,None]*np.exp(-(engergy[1:,None] - energy[0,None])/(temp*8065.313546745508))
 
-
-    if(
     
     return boltz_pop
 
 
+def saha_population(ci, w_z, w_z1, energy_z, energy_z1, ftemp, norm_tot=False):
+
+    z = boltzmann_population(w_z,energy_z,temp,norm_tot = norm_tot)
+    z1 = boltzmann_population(w_z1,energy_z1,temp,norm_tot = norm_tot)
 
 
+    saha = 2*z1/z/ne *( 2*np.pi*temp*8065.313546745508/h**2)**(3/2)
+
+    
 
 
 
