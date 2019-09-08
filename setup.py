c19ee4ced12ca773855ebe0766800b859bdf8026
curtis <caj0026@auburn.edu>
2019-09-08 18:56:01 -0500

    Adding setup file to make colradpy installable I have no idea what is going on here

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='colradpy',
+    version='1.0.0',
+    description='Collisional radiative modeling code suite',
+    url='w'
+    author='Curtis Johnson',
+    keywords='CR',
+    packages=['colradpy'],
+)
+
