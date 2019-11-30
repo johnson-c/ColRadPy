c4bc823bb61d885449bc16011d0ca40a3b745c14
Curt Johnson <caj0026@auburn.edu>
2019-11-30 15:06:49 -0600

    There was a bug for the steady state ionization balance. This bug occurred becuase the solution for SS was taken at some long time. This lead to cases where the time choosen was not long enough to approximate equilibrium. The solution now correctly takes the limit by only using the SS eigenvector, the solution doesn't depend on temperature at all now.

--- colradpy/solve_matrix_exponential.py
+++ colradpy/solve_matrix_exponential.py
@@ -32,10 +32,53 @@ def solve_matrix_exponential(matrix,td_n0,td_t):
     v0 = np.dot(np.linalg.inv(eigenvectors),td_n0)
     vt = v0[:,:,:,None]*np.exp(eigenvals[:,:,:,None]*td_t)
     td_pop = np.einsum('klij,kljt->itkl', eigenvectors, vt)
+
     eigenvals = eigenvals.transpose(2,0,1)
     eigenvectors = eigenvectors.transpose(2,3,0,1)
     return td_pop, eigenvals, eigenvectors
 
+
+def solve_matrix_exponential_steady_state(matrix):
+    """ This definition will solve the steady state solutions of a 4d matrix
+       
+
+       R. LeVeque, Finite Difference Methods for Ordinary and Par-
+       tial Differential Equations: Steady-State and Time-Dependent
+       Problems (Classics in Applied Mathematics Classics in Applied
+       Mathemat), Society for Industrial and Applied Mathematics,
+       Philadelphia, PA, USA, 2007.
+
+
+
+    Args:
+      :param matrix: The 4d matrix to be solved
+      :type matrix: 4d matrix[x,y,temp,dens]
+
+    Returns:
+      This returns three arrays the steady state populations, eigenvals and eigenvectors
+
+    """
+
+    #becuase this is steady state the initial populations don't matter
+    td_n0 = np.zeros(np.shape(matrix)[0]) 
+    td_n0[0] = 1.
+
+    #sort on the eigenval index to find the longest lived state
+    #that will give the equilibrium populuations
+
+    eigenvals, eigenvectors = np.linalg.eig(matrix.transpose(2,3,0,1))
+    v0 = np.dot(np.linalg.inv(eigenvectors),td_n0)
+
+
+    axis=2
+    index = list(np.ix_(*[np.arange(i) for i in eigenvals.shape]))
+    index[axis] = np.abs(eigenvals).argsort(axis)
+
+    
+    ev = eigenvectors.transpose(0,1,3,2)[index]#egienvectors sorted on eigenvals
+    ss_pop = np.einsum('kl,klj->klj',v0[index][:,:,0],ev[:,:,0,:]).transpose(2,0,1)
+    return ss_pop, eigenvals, eigenvectors
+    
 
 def solve_matrix_exponential_source(matrix, td_n0, source, td_t):
     """This definition will solve a 4 dimensional matrix using the matrix expoentiaiation method
