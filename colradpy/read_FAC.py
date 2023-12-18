bcca9086b8e566d4a5dd5a608169f357de03cfe4
cjperks7 <cjperk@mit.edu>
2023-12-18 14:30:06 -0500

    Started formating for FAC file management

--- colradpy/read_FAC.py
+++ colradpy/read_FAC.py
@@ -28,8 +28,41 @@ import numpy as np
 
 def read_FAC(
     # File Management
-    fil = None,
+    fil = None,         # Common path to FAC files, excluding physics extentions
     # Physics controls
     EEDF = None,        # if None -> assumes Maxwell-averages rates from pfac.fac.MaxwellRate
-    physics = None,     # if None -> looks for all
+    physics = None,     # if None -> looks for all file suffixes
     ):
+
+    #### ---- Determines which files to search for ---- ####
+
+    # Electron energy distribution settings
+    if EEDF is None:
+        # Use Maxwell-averaged rates from pfac.fac.MaxwellRate
+        use_mr = True
+    else:
+        print('NON-MAXWELLIAN ELECTRONS NOT IMPLEMENTED YET')
+        sys.exit(1)
+
+    # FAC data file suffixes to search for
+    if physics is None:
+        # If already Maxwell-averaged rate coefficients
+        if use_mr:
+            physics = [
+                'a.en',     # ASCII-format Energy levels
+                'a.tr',     # ASCII-format Einstein coefficients
+                'ce.mr',    # Collisional excitation
+                'rr.mr',    # Radiative recombination
+                #'ai.mr'    # Autoionization/dielectronic recombination
+                'ci.mr',    # Collision ionization
+                ]
+        # If using cross-section files
+        else:
+            physics = [
+                'a.en',      # ASCII-format Energy levels
+                'a.tr',      # ASCII-format Einstein coefficients
+                'a.ce',      # ASCII-format Collisional excitation
+                'a.rr',      # ASCII-format Radiative recombination
+                #'a.ai'      # ASCII-format Autoionization/dielectronic recombination
+                'a.ci',      # ASCII-format Collision ionization
+                ]
